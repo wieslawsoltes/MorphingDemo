bd1ae173b3025bb5daad53c696dbb2c7d3f2782b
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-06-10 22:13:30 +0200

    Update PolyLineSegment.cs

--- MorphingDemo/Avalonia/PolyLineSegment.cs
+++ MorphingDemo/Avalonia/PolyLineSegment.cs
@@ -6,10 +6,10 @@ namespace MorphingDemo.Avalonia
 {
     public sealed class PolyLineSegment : PathSegment
     {
-        public static readonly StyledProperty<AvaloniaList<Point>> PointsProperty
-            = AvaloniaProperty.Register<ArcSegment, AvaloniaList<Point>>(nameof(Points));
+        public static readonly StyledProperty<AvaloniaList<Point>?> PointsProperty
+            = AvaloniaProperty.Register<ArcSegment, AvaloniaList<Point>?>(nameof(Points));
 
-        public AvaloniaList<Point> Points
+        public AvaloniaList<Point>? Points
         {
             get => GetValue(PointsProperty);
             set => SetValue(PointsProperty, value);
@@ -17,15 +17,12 @@ namespace MorphingDemo.Avalonia
 
         protected override void ApplyTo(StreamGeometryContext ctx)
         {
-            if (Points.Count > 0)
+            if (Points?.Count > 0)
             {
-                ctx.BeginFigure(Points[0], false);
                 for (int i = 1; i < Points.Count; i++)
                 {
                     ctx.LineTo(Points[i]);
                 }
-
-                ctx.EndFigure(false);
             }
         }
 
