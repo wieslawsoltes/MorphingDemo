a67cb8308caeda4a97a33938ca58b773d5ed9612
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-06-10 20:39:33 +0200

    Flatten

--- MorphingDemo/MainWindow.axaml.cs
+++ MorphingDemo/MainWindow.axaml.cs
@@ -17,11 +17,14 @@ namespace MorphingDemo
 
             var source = PathGeometry.Parse("M3 6C3 4.34315 4.34315 3 6 3H14C15.6569 3 17 4.34315 17 6V14C17 15.6569 15.6569 17 14 17H6C4.34315 17 3 15.6569 3 14V6ZM6 4C4.89543 4 4 4.89543 4 6V14C4 15.1046 4.89543 16 6 16H14C15.1046 16 16 15.1046 16 14V6C16 4.89543 15.1046 4 14 4H6Z");
 
-            source.Figures
-            
+            var sourceFlattened = source.FlattenWithTransform(p => p, FlattenOutput.PolyLines);
+
             var target = PathGeometry.Parse("M10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3ZM2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10Z");
-            
-            var cache = Morph.ToCache(source, target, 0.01);
+
+            var targetFlattened = target.FlattenWithTransform(p => p, FlattenOutput.PolyLines);
+
+            var cache = Morph.ToCache(sourceFlattened, targetFlattened, 0.01);
+
         }
 
         private void InitializeComponent()
