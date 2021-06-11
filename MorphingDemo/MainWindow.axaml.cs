b163760fb4934285e1c4f43cb8c765113634a62f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-06-11 22:31:16 +0200

    Update MainWindow.axaml.cs

--- MorphingDemo/MainWindow.axaml.cs
+++ MorphingDemo/MainWindow.axaml.cs
@@ -17,9 +17,7 @@ namespace MorphingDemo
             this.AttachDevTools();
 #endif
 
-            //var source = PathGeometry.Parse("M3 6C3 4.34315 4.34315 3 6 3H14C15.6569 3 17 4.34315 17 6V14C17 15.6569 15.6569 17 14 17H6C4.34315 17 3 15.6569 3 14V6ZM6 4C4.89543 4 4 4.89543 4 6V14C4 15.1046 4.89543 16 6 16H14C15.1046 16 16 15.1046 16 14V6C16 4.89543 15.1046 4 14 4H6Z");
-
-            var source = PathGeometry.Parse("M0 0L15 0L15 15L0 15L0 0Z");
+            var source = PathGeometry.Parse("M3 6C3 4.34315 4.34315 3 6 3H14C15.6569 3 17 4.34315 17 6V14C17 15.6569 15.6569 17 14 17H6C4.34315 17 3 15.6569 3 14V6ZM6 4C4.89543 4 4 4.89543 4 6V14C4 15.1046 4.89543 16 6 16H14C15.1046 16 16 15.1046 16 14V6C16 4.89543 15.1046 4 14 4H6Z");
             var sourceFlattened = source.Flatten(FlattenOutput.PolyLines);
             //var sourceFlattened = source.Flatten(FlattenOutput.Lines);
 
