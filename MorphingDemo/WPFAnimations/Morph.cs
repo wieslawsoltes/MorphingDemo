8ae9c755b3b2a31f13fe2054a474f55720245f6e
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-06-12 21:19:08 +0200

    Update Morph.cs

--- MorphingDemo/WPFAnimations/Morph.cs
+++ MorphingDemo/WPFAnimations/Morph.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Avalonia;
 using Avalonia.Animation.Easings;
-using Avalonia.Collections;
 using Avalonia.Media;
 
 namespace WPFAnimations
