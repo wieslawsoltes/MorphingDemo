032465ffb50bedb95d9956a8322ae23b4cbacfbd
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-06-12 21:38:42 +0200

    Create GeometryAnimator.cs

---
+++ PolyLineAnimation/GeometryAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Animation.Animators;
+using Avalonia.Data;
+using Avalonia.Media;
+
+namespace PolyLineAnimation
+{
+    public class GeometryAnimator : Animator<Geometry>
+    {
+        public override Geometry Interpolate(double progress, Geometry oldValue, Geometry newValue)
+        {
+            var clone = (oldValue as PathGeometry).ClonePathGeometry();
+
+            Morph.To(clone, newValue as PathGeometry, progress);
+
+            return clone;
+        }
+    }
+}
